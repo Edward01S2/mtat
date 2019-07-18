77a3e578c43e284df116ba245d8d5cb39a915b78
edward01s2 <edward.01s2@gmail.com>
2019-07-18 08:07:38 -0500

    Project on pause

--- resources/views/partials/programs.blade.php
+++ resources/views/partials/programs.blade.php
@@ -35,9 +35,9 @@
       <div class="flex flex-col md:flex-row">
         @foreach($programs->program_no_image as $program)
         <a class="md:w-1/3 relative border-b-2 border-white scale-up" href="{{$program->link->url}}">
-
+          <div class="relative">
             <div class="absolute h-full w-full z-0 hover:shadow-inner" style="background-color: {{ $program->bg_color}}; opacity: {{$program->opacity}};"></div>
-            <div class="relative z-20 flex justify-between px-8 py-6 md:pl-8 md: pr-4 md:h-full">
+            <div class="relative z-20 flex justify-between px-8 py-6 md:pl-8 md: pr-4">
               <div class="text-left flex items-center md:flex-col md:items-start md:justify-center">
                 <h3 class="uppercase text-lg mr-8 mb-0 font-semibold tracking-wider text-blue-800 md:mr-0 md:pb-2 lg:text-lg">{!!$program->name!!}</h3>
                 <p class="hidden text-gray-700 font-light md:block">{{$program->subtext}}</p>
@@ -46,6 +46,7 @@
                 <svg class="fill-current text-mtat-orange h-6 w-6" xmlns="http://www.w3.org/2000/svg" height="1" width="1" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
               </div>
             </div>
+          </div>
 
         </a>
         @endforeach
